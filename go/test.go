35d46dd004f7cae47a94753f2d84fd3834c17681
yup_gui071 <2261099071@qq.com>
2024-04-23 22:05:55 +0800

    go learning 4.22

--- go/test.go
+++ go/test.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	myStr := []string{"MYBLOG", "NAME"}
 	for i, v := range myStr { //这里理解成把v赋值成myStr的个个值
-		fmt.Printf("当此循环：%v", i)
+		fmt.Printf("当此循环：%v\n", i)
 		fmt.Printf("输出对象：%v\n", v)
 	}
 }
