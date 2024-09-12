cb8116f764358073a1b51ec1d37695cba24ddc23
yup_gui071 <2261099071@qq.com>
2024-09-12 16:13:57 +0800

    py learning 9.12

---
+++ py/chap2/eg.py
@@ -0,0 +1,5 @@
+fp = open("note.txt", "w")  # 打开文件 w-->write r-->read a-->add
+print("原神，启动！", file=fp)
+fp.close()
+a = 100
+print("a")
--- py/chap2/示例2-5使用print函数将数据输出到文件中.py
+++
@@ -1,3 +0,0 @@
-fp=open("note.txt","w") #打开文件 w-->write r-->read a-->add
-print("原神，启动！",file=fp)
-fp.close()
