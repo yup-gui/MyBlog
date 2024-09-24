1794db3143d521bc0ce0911041a15e4fb16ce962
yup_gui071 <2261099071@qq.com>
2024-09-24 10:52:50 +0800

    py formatting

--- py/chap2/eg.py
+++ py/chap2/eg.py
@@ -1,5 +1,4 @@
-fp = open("note.txt", "w")  # 打开文件 w-->write r-->read a-->add
+fp=open("note.txt", "w")  # 打开文件 w-->write r-->read a-->add
 print("原神，启动！", file=fp)
 fp.close()
-a = 100
-print("a")
+
